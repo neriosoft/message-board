81e9edc325ce4e0a979ae4436f4a782c729a59e2
neriosoft <alabaneri@gmail.com>
2023-10-14 21:35:32 +0100

    Added files for vercel deployment and posgre database setting

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-eoq5$$7u$u)u1kt(cl49vjyd0%_q8_kx=@a#r2))t#ph4!^^#!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com","localhost","127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app","now.sh", "127.0.0.1","localhost"]
 
 # Application definition
 
@@ -73,12 +74,24 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "messagedb",
+        "USER": "neriosoft1",
+        "PASSWORD": "Newcreation",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
+
 
 
 # Password validation
@@ -116,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
+
+MEDIA_URL = "img/"
+MEDIA_ROOT = BASE_DIR/"media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
